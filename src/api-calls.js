1e332a96ca0414d86af7586503ea1982447457e0
Ryan Murry <ryanjmurry@gmail.com>
2018-08-10 10:31:11 -0700

    create file, add apiCalls class, and add method to get all conditions and method to get doctors by condition

---
+++ src/api-calls.js
@@ -0,0 +1,32 @@
+export class ApiCalls {
+  getConditions() {
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/conditions?user_key=${process.env.exports.apiKey}`;
+      request.onload = () => {
+        if (request.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusTest));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();   
+    });
+  }
+  
+  getDoctorByCondition (condition) {
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=45.5206223%2C-122.6773984%2C100&user_location=45.5206223%2C-122.6773984&fields=practices%2Cprofile&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = () => {
+        if (request.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusTest));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
