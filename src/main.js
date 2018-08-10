323f7ed8100a54ca3a3e357fd33f10ddb5b76af8
Ryan Murry <ryanjmurry@gmail.com>
2018-08-10 15:04:29 -0700

    add logic to populate conditions dropdown selection list

--- src/main.js
+++ src/main.js
@@ -2,7 +2,22 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { ApiCall } from './api-call.js';
+import { ApiParse } from './api-parse.js';
 
 $(document).ready(function() {
+  //populates conditions drop down list
+  let newApiCall = new ApiCall();
+  let conditionsPromise = newApiCall.ConditionsPromise();
+
+  conditionsPromise.then(function(response) {
+    let newParse = new ApiParse();
+    let allConditions = newParse.getConditions(response);
+    allConditions.forEach(function(condition) {
+      $('#conditionSelector').append(`<option>${condition}</option>`);
+    })
+  });
+
+  
   
 });
