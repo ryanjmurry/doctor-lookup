5b6782127f0f53291af155396f8e5bb38405dfec
Ryan Murry <ryanjmurry@gmail.com>
2018-08-10 12:34:12 -0700

    update method names and change url to sort returned doctors by ascending last names

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,5 +1,5 @@
 export class ApiCalls {
-  getConditions() {
+  getConditionsPromise() {
     return new Promise((resolve, reject) => {
       let request = new XMLHttpRequest();
       let url = `https://api.betterdoctor.com/2016-03-01/conditions?user_key=${process.env.exports.apiKey}`;
@@ -15,10 +15,10 @@ export class ApiCalls {
     });
   }
   
-  getDoctorByCondition(condition) {
+  getDoctorByConditionPromise(condition) {
     return new Promise((resolve, reject) => {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=45.5206223%2C-122.6773984%2C50&user_location=45.5206223%2C-122.6773984&fields=practices%2Cprofile%2Cspecialties&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=45.5206223%2C-122.6773984%2C50&user_location=45.5206223%2C-122.6773984&sort=last-name-asc&fields=practices%2Cprofile%2Cspecialties&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = () => {
         if (request.status === 200) {
           resolve(request.response);
@@ -31,10 +31,10 @@ export class ApiCalls {
     });
   }
 
-  getDoctorByLastName(lastName)  {
+  getDoctorByLastNamePromimse(lastName)  {
     return new Promise((resolve, reject) => {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?last_name=${lastName}&location=45.5206223%2C-122.6773984%2C50&user_location=45.5206223%2C-122.6773984&fields=practices%2Cprofile%2Cspecialties&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?last_name=${lastName}&location=45.5206223%2C-122.6773984%2C50&user_location=45.5206223%2C-122.6773984&sort=last-name-asc&fields=practices%2Cprofile%2Cspecialties&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = () => {
         if (request.status === 200) {
           resolve(request.response);
