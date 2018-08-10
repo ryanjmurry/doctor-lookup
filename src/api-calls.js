6e03fe24b9a2060be13ccec6e50418f91c63600e
Ryan Murry <ryanjmurry@gmail.com>
2018-08-10 10:37:19 -0700

    change range of search from 100 miles to 50 miles

--- src/api-calls.js
+++ src/api-calls.js
@@ -15,10 +15,10 @@ export class ApiCalls {
     });
   }
   
-  getDoctorByCondition (condition) {
+  getDoctorByCondition(condition) {
     return new Promise((resolve, reject) => {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=45.5206223%2C-122.6773984%2C100&user_location=45.5206223%2C-122.6773984&fields=practices%2Cprofile&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=45.5206223%2C-122.6773984%2C50&user_location=45.5206223%2C-122.6773984&fields=practices%2Cprofile&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = () => {
         if (request.status === 200) {
           resolve(request.response);
@@ -30,3 +30,6 @@ export class ApiCalls {
       request.send();
     });
   }
+
+  getDoctorByLastName() 
+}
