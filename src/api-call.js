025773fc80d68b4f693f728fc84fec9709915154
Ryan Murry <ryanjmurry@gmail.com>
2018-08-10 16:02:34 -0700

    update method names

--- src/api-call.js
+++ src/api-call.js
@@ -1,5 +1,5 @@
 export class ApiCall {
-  ConditionsPromise() {
+  conditionsPromise() {
     debugger;
     return new Promise((resolve, reject) => {
       let request = new XMLHttpRequest();
@@ -16,7 +16,7 @@ export class ApiCall {
     });
   }
   
-  DoctorByConditionPromise(condition) {
+  drByConditionPromise(condition) {
     return new Promise((resolve, reject) => {
       let request = new XMLHttpRequest();
       let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=45.5206223%2C-122.6773984%2C50&user_location=45.5206223%2C-122.6773984&sort=last-name-asc&fields=practices%2Cprofile%2Cspecialties%2Cuid&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
@@ -32,7 +32,7 @@ export class ApiCall {
     });
   }
 
-  DoctorByLastNamePromimse(lastName)  {
+  drByLastNamePromimse(lastName)  {
     return new Promise((resolve, reject) => {
       let request = new XMLHttpRequest();
       let url = `https://api.betterdoctor.com/2016-03-01/doctors?last_name=${lastName}&location=45.5206223%2C-122.6773984%2C50&user_location=45.5206223%2C-122.6773984&sort=last-name-asc&fields=practices%2Cprofile%2Cspecialties%2Cuid&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
