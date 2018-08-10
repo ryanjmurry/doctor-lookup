f7a9ad950f1744a05d4bf509dadbf48b052cb2a5
Ryan Murry <ryanjmurry@gmail.com>
2018-08-10 11:27:22 -0700

    update URL to expand search to return doctor specialties

--- src/api-calls.js
+++ src/api-calls.js
@@ -18,7 +18,7 @@ export class ApiCalls {
   getDoctorByCondition(condition) {
     return new Promise((resolve, reject) => {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=45.5206223%2C-122.6773984%2C50&user_location=45.5206223%2C-122.6773984&fields=practices%2Cprofile&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=45.5206223%2C-122.6773984%2C50&user_location=45.5206223%2C-122.6773984&fields=practices%2Cprofile%2Cspecialties&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = () => {
         if (request.status === 200) {
           resolve(request.response);
@@ -34,7 +34,7 @@ export class ApiCalls {
   getDoctorByLastName(lastName)  {
     return new Promise((resolve, reject) => {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?last_name=${lastName}&location=45.5206223%2C-122.6773984%2C50&user_location=45.5206223%2C-122.6773984&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?last_name=${lastName}&location=45.5206223%2C-122.6773984%2C50&user_location=45.5206223%2C-122.6773984&fields=practices%2Cprofile%2Cspecialties&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = () => {
         if (request.status === 200) {
           resolve(request.response);
