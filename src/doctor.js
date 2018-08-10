1699b86c72b911552e31f16d11be6bf7d4c68efc
Ryan Murry <ryanjmurry@gmail.com>
2018-08-10 16:04:29 -0700

    add website to Doctor class constructor

--- src/doctor.js
+++ src/doctor.js
@@ -1,11 +1,12 @@
 export class Doctor {
-  constructor(doctorId, firstName, lastName, bio, specialties, practiceName, address, phoneNumber, newPatients) {
+  constructor(doctorId, firstName, lastName, bio, specialties = "", practiceName, website = "", address, phoneNumber, newPatients) {
     this.doctorId = doctorId;
     this.firstName = firstName;
     this.lastName = lastName;
     this.bio = bio;
     this.specialties = specialties;
     this.practiceName = practiceName;
+    this.website = website;
     this.address = address;
     this.phoneNumber = phoneNumber;
     this.newPatients = newPatients;
