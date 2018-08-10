dbebda375bab85ae908f55a2ccdc54a3f510c097
Ryan Murry <ryanjmurry@gmail.com>
2018-08-10 11:30:39 -0700

    create practice.js file with Practice class and constructor

---
+++ src/practice.js
@@ -0,0 +1,8 @@
+export class Practice {
+  constructor(name, address, phoneNumber, newPatients) {
+    this.name = name;
+    this.address = address;
+    this.phoneNumber = phoneNumber;
+    this.newPatients = newPatients;
+  }
+}
