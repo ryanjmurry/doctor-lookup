9082fae533261eb3e93bcba7cebe62756b797c32
Ryan Murry <ryanjmurry@gmail.com>
2018-08-10 10:51:54 -0700

    add doctor.js and create Doctor class and constructor

---
+++ src/doctor.js
@@ -0,0 +1,9 @@
+export class Doctor {
+  constructor(firstName, lastName, address, phoneNumber,  newPatients) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.phoneNumber = phoneNumber;
+    this.newPatients = newPatients;
+  }
+}
