9661b9d1ec49b7664cf8872ddef4abd26424f2f6
Ryan Murry <ryanjmurry@gmail.com>
2018-08-10 10:40:55 -0700

    add method to get doctors by last name

--- src/api-calls.js
+++ src/api-calls.js
@@ -31,5 +31,19 @@ export class ApiCalls {
     });
   }
 
-  getDoctorByLastName() 
+  getDoctorByLastName(lastName)  {
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?last_name=${lastName}&location=45.5206223%2C-122.6773984%2C50&user_location=45.5206223%2C-122.6773984&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = () => {
+        if (request.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusTest));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
 }
