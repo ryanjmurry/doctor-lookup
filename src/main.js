482362a0f3bbc4ac021980429b8d114a7b6a9878
Ryan Murry <ryanjmurry@gmail.com>
2018-08-11 01:06:33 -0700

    add search funtionality by doctor last name and alter new patent acceptance icon to only show if practices are not accepting

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import './styles.css';
 import { ApiCall } from './api-call.js';
 import { ApiParse } from './api-parse.js';
 
+
+
 $(document).ready(function() {
   //populates conditions drop down list
   let newApiCall = new ApiCall();
@@ -19,9 +21,11 @@ $(document).ready(function() {
   });
 
   $('#findDrByCondition').click(function() {
+    $('.results').show();
     //get condition and clear results area
     let condition = $('#conditionSelector').val();
     $('#drListGroup').text("");
+    // prepareResultsArea();
 
     //make new API Call
     let newApiCall = new ApiCall();
@@ -31,40 +35,21 @@ $(document).ready(function() {
     drByConditionPromise.then(function(response) {
       let newParse = new ApiParse();
       let foundDoctors = newParse.getDoctors(response);
-      console.log(foundDoctors);
+      console.log(foundDoctors.length);
       //appends doctor info to doctor list
       foundDoctors.forEach(function(doctor) {
         //to display site URL if one is retrieved from database
         let siteUrl = "";
-        if (doctor.website !== "")
-        {
+        if (doctor.website !== "") {
           siteUrl += `<a href="${doctor.website}" class="info-link"><i class="fas fa-external-link-alt"></i> Go To Site</a>`;
         }
 
-        /*
+        //to display icon if practice is not accepting new patients
+        let newPatient = "";
+        if (doctor.newPatients === false) {
+          newPatient += `<i class="fas fa-user-alt-slash"></i>`
+        }
 
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">
-                ...
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        */
-        
         //append found doctors to results page
         $('#drListGroup').append(`
         <div class="modal fade" id="${doctor.doctorId}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
@@ -77,11 +62,10 @@ $(document).ready(function() {
                 </button>
               </div>
               <div class="modal-body">
-                <h4>Specialties</h4>
+                <h4>Bio</h4>
                 ${doctor.bio}
               </div>
               <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
               </div>
             </div>
           </div>
@@ -90,7 +74,7 @@ $(document).ready(function() {
           <div class="row">
             <div class="col">
               <h4 class="doctor-name"> ${doctor.lastName}, ${doctor.firstName}</h4><a href="#" data-toggle="modal" data-target="#${doctor.doctorId}"><i class="fas fa-info-circle"></i></a>
-              <h6>${doctor.practiceName}</h6>
+              <h6 class="practice-name">${doctor.practiceName}</h6>${newPatient}
               ${doctor.specialties.map(function(specialty){
                 return "<p>" + specialty.name + "</p>"
               }).join('')}
@@ -116,5 +100,90 @@ $(document).ready(function() {
       $('.error').show();
       $('.error').text(`There was an error: ${error.message}`);      
     });
-  });  
+  });
+  
+  $('#findDrByLastName').click(function() {
+    $('.results').show();
+    //get condition and clear results area
+    let lastName = $('#drLastName').val();
+    $('#drListGroup').text("");
+    // prepareResultsArea();
+
+    //make new API Call
+    let newApiCall = new ApiCall();
+    let drByLastNamePromise = newApiCall.drByLastNamePromimse(lastName);
+
+    //execute promise
+    drByLastNamePromise.then(function(response) {
+      let newParse = new ApiParse();
+      let foundDoctors = newParse.getDoctors(response);
+      console.log(foundDoctors);
+      //appends doctor info to doctor list
+      foundDoctors.forEach(function(doctor) {
+        //to display site URL if one is retrieved from database
+        let siteUrl = "";
+        if (doctor.website !== "") {
+          siteUrl += `<a href="${doctor.website}" class="info-link"><i class="fas fa-external-link-alt"></i> Go To Site</a>`;
+        }
+
+        //to display 
+        console.log(typeof doctor.newPatients)
+        let newPatient = "";
+        if (doctor.newPatients === true) {
+          newPatient += `<i class="fas fa-user-alt-slash"></i>`
+        }
+
+        //append found doctors to results page
+        $('#drListGroup').append(`
+        <div class="modal fade" id="${doctor.doctorId}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h2 class="modal-title" id="exampleModalLongTitle">${doctor.firstName} ${doctor.lastName}</h2>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <h4>Specialties</h4>
+                ${doctor.bio}
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <li class="list-group-item">
+          <div class="row">
+            <div class="col">
+              <h4 class="doctor-name"> ${doctor.lastName}, ${doctor.firstName}</h4><a href="#" data-toggle="modal" data-target="#${doctor.doctorId}"><i class="fas fa-info-circle"></i></a>
+              <h6 class="practice-name">${doctor.practiceName}</h6>${newPatient}
+              ${doctor.specialties.map(function(specialty){
+                return "<p>" + specialty.name + "</p>"
+              }).join('')}
+            </div>
+            <div class="col">
+              ${doctor.phoneNumber.map(function(number){
+                if (number.type === "landline") {
+                  return `<a href="tel:${number.number}" class="info-link"><i class="fas fa-phone"></i> Call ${number.number}<a>`
+                }
+              }).join('')}
+              <div>
+                <i class="fas fa-map-marked-alt"></i><a href="http://maps.google.com/?q=${doctor.address.street}, ${doctor.address.city}, ${doctor.address.state} ${doctor.address.zip}" class="info-link"> Address</a>
+              </div>
+              <div>
+                ${siteUrl}
+              </div>
+            </div>
+          </div>
+        </li>`)
+      })
+    }, function(error) {
+      $('.results').hide();
+      $('.error').show();
+      $('.error').text(`There was an error: ${error.message}`);      
+    });
+  });
+
 });
