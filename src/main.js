b690d87498c21d46b3c5b49804c15f3bfe15c0b6
Ryan Murry <ryanjmurry@gmail.com>
2018-08-10 23:53:54 -0700

    add modal to display doctor bio

--- src/main.js
+++ src/main.js
@@ -41,12 +41,55 @@ $(document).ready(function() {
           siteUrl += `<a href="${doctor.website}" class="info-link"><i class="fas fa-external-link-alt"></i> Go To Site</a>`;
         }
 
+        /*
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                ...
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        */
+
         //append found doctors to results page
         $('#drListGroup').append(`
+        <div class="modal fade" id="${doctor.doctorId}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">${doctor.firstName} ${doctor.lastName}</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <h4>Specialties</h4>
+                ${doctor.bio}
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
         <li class="list-group-item">
           <div class="row">
             <div class="col">
-              <h4 class="doctor-name"> ${doctor.lastName}, ${doctor.firstName}</h4><i class="fas fa-info-circle"></i>
+              <h4 class="doctor-name"> ${doctor.lastName}, ${doctor.firstName}</h4><a href="#" data-toggle="modal" data-target="#${doctor.doctorId}"><i class="fas fa-info-circle"></i></a>
               <h6>${doctor.practiceName}</h6>
               ${doctor.specialties.map(function(specialty){
                 return "<p>" + specialty.name + "</p>"
