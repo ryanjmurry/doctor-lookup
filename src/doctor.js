83827d0764c648650456e067077d6b27973fa122
Ryan Murry <ryanjmurry@gmail.com>
2018-08-13 15:46:33 -0700

    update constructor to take doctor image as parameter

--- src/doctor.js
+++ src/doctor.js
@@ -1,9 +1,10 @@
 export class Doctor {
-  constructor(doctorId, firstName, lastName, bio, specialties = "", practiceName, website = "", address, phoneNumber, newPatients) {
+  constructor(doctorId, firstName, lastName, bio, image, specialties = "", practiceName, website = "", address, phoneNumber, newPatients) {
     this.doctorId = doctorId;
     this.firstName = firstName;
     this.lastName = lastName;
     this.bio = bio;
+    this.image = image;
     this.specialties = specialties;
     this.practiceName = practiceName;
     this.website = website;
