a5d781946de95691ded33f088a1a76b08c00d585
Ryan Murry <ryanjmurry@gmail.com>
2018-08-13 16:31:44 -0700

    update api call to take in a city as a parameter

--- src/api-call.js
+++ src/api-call.js
@@ -15,10 +15,10 @@ export class ApiCall {
     });
   }
   
-  drByConditionPromise(condition) {
+  drByConditionPromise(condition, city) {
     return new Promise((resolve, reject) => {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=45.5206223%2C-122.6773984%2C50&user_location=45.5206223%2C-122.6773984&sort=last-name-asc&fields=practices%2Cprofile%2Cspecialties%2Cuid&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=${city}&sort=last-name-asc&fields=practices%2Cprofile%2Cspecialties%2Cuid&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = () => {
         if (request.status === 200) {
           resolve(request.response);
@@ -31,10 +31,10 @@ export class ApiCall {
     });
   }
 
-  drByLastNamePromimse(lastName)  {
+  drByLastNamePromimse(lastName, city)  {
     return new Promise((resolve, reject) => {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?last_name=${lastName}&location=45.5206223%2C-122.6773984%2C50&user_location=45.5206223%2C-122.6773984&sort=last-name-asc&fields=practices%2Cprofile%2Cspecialties%2Cuid&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?last_name=${lastName}&location=${city}&sort=last-name-asc&fields=practices%2Cprofile%2Cspecialties%2Cuid&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = () => {
         if (request.status === 200) {
           resolve(request.response);
