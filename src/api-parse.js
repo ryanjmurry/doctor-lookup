ad14139fbab0b058dd38f52a9941794c32536c56
Ryan Murry <ryanjmurry@gmail.com>
2018-08-13 22:53:57 -0700

    remove constructor comment

--- src/api-parse.js
+++ src/api-parse.js
@@ -14,7 +14,6 @@ export class ApiParse {
   getDoctors(response) {
     let body = JSON.parse(response);
     let allDoctors = [];
-    //  constructor(doctorId, firstName, lastName, bio, specialties, practiceName, website, address, phoneNumber, newPatients) {
     body.data.forEach(function(doctor) {
       let foundDoctor = new Doctor(doctor.uid, doctor.profile.first_name, doctor.profile.last_name, doctor.profile.bio, doctor.profile.image_url, doctor.specialties, doctor.practices[0].name, doctor.practices[0].website, doctor.practices[0].visit_address, doctor.practices[0].phones, doctor.practices[0].accepts_new_patients);
       allDoctors.push(foundDoctor);
